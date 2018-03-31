10a882d7e8fc9385832125281640360a0f0d88a6
Mari0 <mario.hi52@gmail.com>
2018-03-31 17:17:07 +0200

    Added date inserter -> V0.0.2

--- extension.js
+++ extension.js
@@ -1,10 +1,10 @@
 const vscode = require('vscode');
 
-function addZero(i) { return i < 10 ? "0"+i : i; }
+function addZero(i) { return i < 10 ? "0"+i.toString() : i; }
 
 function activate(context) {
     const config = vscode.workspace.getConfiguration('timeinserter');
-    let disposable = vscode.commands.registerCommand('timeinserter.insert', function () {
+    let disposable1 = vscode.commands.registerCommand('timeinserter.insertTime', function () {
         let editor = vscode.window.activeTextEditor;
         if (!editor) {
             return;
@@ -26,14 +26,32 @@ function activate(context) {
 
         // vscode.window.showInformationMessage(dateText);
         // vscode.window.showInformationMessage(JSON.stringify(position, null, 4));
-        // var doc = editor.document;
+
+        editor.edit(function (editor) {
+            editor.insert(position, dateText);
+        });
+    });
+    let disposable2 = vscode.commands.registerCommand('timeinserter.insertDate', function () {
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+
+        var date   = new Date();
+        var day    = date.getDate();
+        var month  = date.getMonth()+1;
+        var year   = date.getFullYear();
+
+        var dateText = `${config.pre}${addZero(day).toString()}${config.dateSeparator}${addZero(month).toString()}${config.dateSeparator}${year.toString()}${config.post}`;
+        
+        const position = editor.selection.active;
 
         editor.edit(function (editor) {
             editor.insert(position, dateText);
         });
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push([disposable1, disposable2]);
 }
 exports.activate = activate;
 
